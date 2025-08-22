b0ccb89e15db80b5edb4c094c1640b6f813123f4
ychen266 <chenyiyanguk@163.com>
2025-08-22 22:00:40 +1000

    finished week 3 1-6

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,16 @@
 // import TheWelcome from './components/TheWelcome.vue'
 import MyName from './components/MyName.vue'
 import JSON from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <MyName />
-      <JSON />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <!--<JSON /> -->
+  <!--<JSONSolution /> -->
+  <Form />
 </template>
+
+
 
 <style scoped>
 header {
